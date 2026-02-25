9aff88945330ef690309e8ca00a4d4ee12f83b2f
Thanmayee <kktanmayee2018@gmail.com>
2026-02-25 13:54:04 +0530

    db to sqlite

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-# Replace with your MySQL credentials
-DATABASE_URL = "mysql+pymysql://root:2005@localhost/resume_gpt"
+DATABASE_URL = "sqlite:///./resume_gpt.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# import os
+
+# # Replace with your MySQL credentials
+# DATABASE_URL = "mysql+pymysql://root:2005@localhost/resume_gpt"
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base = declarative_base()
